5a9ec506e355a41186a0ec379acb12aad9aefbed
rakave <velu.ravichandran@chicagobooth.edu>
2015-12-11 16:51:35 -0600

    Validate Favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,7 @@ class Favorite < ActiveRecord::Base
   belongs_to :book , :class_name => "Book", :foreign_key => "book_id"
   belongs_to :user , :class_name => "User", :foreign_key => "user_id"
 
+  validates :user, :presence => true
+  validates :book, :presence => true, :uniqueness => { :scope => :user }
+
 end
